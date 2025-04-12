7a2de83da4c9ac106c68e633ede0a379ac79c05e
Prasanna_Patil <prasannapatil365@gmil.com>
2025-04-13 00:40:56 +0530

    (#07 updated_callbackuri)

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.use((err, req, res, next) => {
 
 // Start Server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running `);
 });
 
 
